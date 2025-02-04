16a89611e981d3c36441520113f68ca671630af7
Roma Pleshenko <romaple28@gmail.com>
2025-02-04 15:16:38 +0200

    commit

--- H.W.5/7.3/script.js
+++ H.W.5/7.3/script.js
@@ -1,10 +1,16 @@
 function hundred () {
+    let input;
     for (let i=0; i<10; i++){
-        let input = prompt('введите число больще 100');
-        if (input>100){
-            console.log(input);
-            break;
+        input = prompt('введите число больще 100');
+        if (input === null) {
+            console.log('отменили ввод');
+            return;
+        }
+        if (input>100) {
+            console.log(`число больше ста : ${input}`)
+            return;
         }
     }
+    console.log(`последние число  ${input}`)
 }
 hundred();
