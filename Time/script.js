c21373e9c2a8bc947dffbd727af5979ff0e8f179
Roma Pleshenko <romaple28@gmail.com>
2025-03-10 19:19:50 +0200

    commit timer

--- Time/script.js
+++ Time/script.js
@@ -1,11 +1,22 @@
 const timer = document.getElementById("timer");
-let time = prompt("Введите время в секундах");
 
-setInterval(update,1000);
+let time;
+do {
+    time = prompt("Введите время в секундах");
+} while (isNaN(time) || time === null || time.trim() === "" || time < 0);
+
+time = Number(time);
+
 function update() {
-    const minutes = Math.floor(time/60);
-    let second = time%60;
-    second = second < 10 ? "0" + second: second;
-    timer.innerHTML = `${minutes}:${second}`;
-    time--;
+    if (time >= 0) {
+        const minutes = Math.floor(time / 60);
+        let second = time % 60;
+        second = second < 10 ? "0" + second : second;
+        timer.innerHTML = `${minutes}:${second}`;
+        time--;
+    } else {
+        clearInterval(interval);
+    }
 }
+
+const interval = setInterval(update, 1000);
